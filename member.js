fe400ab462fd3b0c5823a6f8bee01996103b7573
Randy O'Neil <rano@outlook.com>
2025-01-23 18:54:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  var skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "jQuery",
+    "Bootstrap",
+    "PHP",
+    "MySQL",
+    "MongoDB",
+    "C#",
+    ".NET",
+    "Python",
+    "Java",
+    "C++",
+    "C",
+    "Swift",
+    "Kotlin",
+    "Objective-C",
+        "R"
+      ];
+    }
